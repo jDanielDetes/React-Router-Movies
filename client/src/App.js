55c2338466352741d16158ce03500f383ecc3e6e
jDanielDetes <jdanieldetes@gmail.com>
2019-12-17 16:51:46 -0500

    part 1 50% done

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import {Route} from "react-router-dom"
-import { MovieList} from "./Movies/MovieList"
+import {MovieList} from "./Movies/MovieList"
+import {Movie}  from "./Movies/Movie"
+
 
 import SavedList from './Movies/SavedList';
 
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const Movie = (props) => {
+export const Movie = (props) => {
   const [movie, setMovie] = useState();
  
   useEffect(() => {
